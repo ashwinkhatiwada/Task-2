515a13595b30be458a9dac672c10a2364430a483
ashwin1321 <ashwin.khatiwada@globalyhub.com>
2023-03-24 15:06:53 +0545

    feat: get data from the api

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const riotApi = process.env.API_URL;
+const baseUrl = "https://eu.api.riotgames.com/val/content/v1/contents"
+
+const getData = async () => {
+    const url = `${baseUrl}?api_key=${riotApi}`;
+
+    try {
+        const response = await axios.get(url)
+            .then((res) => {
+                const data = JSON.stringify(res.data);
+                console.log(data);
+            })
+    } catch (error) {
+        console.error
+
+    }
+
+
+
+}
+
+getData();
