fda81380554d671f155477149c6d124ad56c2cdb
ashwin1321 <ashwin.khatiwada@globalyhub.com>
2023-03-24 16:48:30 +0545

    feat: get data from the api, save in local and destructure the data

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ dotenv.config();
 const riotApi = process.env.API_URL;
 const baseUrl = "https://eu.api.riotgames.com/val/content/v1/contents"
 
+let datas;
 const getData = async () => {
     const url = `${baseUrl}?api_key=${riotApi}`;
-
     try {
         const response = await axios.get(url)
             .then((res) => {
-                const data = JSON.stringify(res.data);
-                fs.writeFileSync("data.json", data, (err) => {
-                    if (!err) {
-                        console.log("Data saved")
-                    }
-                })
+
+                if (res.status === 429) {               // if api exceeds the limit, read the data from the file
+                    const readData = JSON.parse(fs.readFileSync("output.json"));
+                    datas = readData;
+                    return
+                }
+                else {
+                    datas = (res.data);
+                    return
+                }
             })
     } catch (error) {
         console.error
-
     }
-
-
-
 }
+await getData();
 
-getData();
+const { characters, skins, maps, chromas, skinLevels, equips, gameModes, sprays, sprayLevels, playerCards, playerTitles, ceremonies } = datas
