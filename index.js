2813b0b5f1f330db83dd764d3e44beb4deab15a2
ashwin1321 <ashwin.khatiwada@globalyhub.com>
2023-03-29 11:33:21 +0545

    feat: add welcome message

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-import axios from "axios";
-import dotenv from "dotenv";
-import fs from "fs";
-dotenv.config();
+import data from "./api/getData.js";
+import chalk from "chalk";
+import readline from "readline";
 
-const riotApi = process.env.API_URL;
-const baseUrl = "https://eu.api.riotgames.com/val/content/v1/contents"
-
-const getData = async () => {
-    let datas;
-    const url = `${baseUrl}?api_key=${riotApi}`;
-    try {
-        const response = await axios.get(url)
-        console.log(response.status)
-
-        if (response.status === 200) {               // if api is working, write the data to the file
-            datas = await response.data;
-            return datas
-        }
-
-        else if (response.status === 429) {               // if api exceeds the limit, read the data from the file
-            const readData = JSON.parse(fs.readFileSync("output.json"));
-            datas = readData;
-            return datas
-        }
-        else {
-            console.error
-        }
-    } catch (error) {
-        console.error
-    }
-}
-
-const data = await getData();
-
+// destructure the data object
 const { characters, skins, maps, chromas, skinLevels, equips, gameModes, sprays, sprayLevels, playerCards, playerTitles, ceremonies } = data
 
-console.log(characters)
+console.group();
+console.log(chalk.red(`WELCOME TO VALORANT.
+Valorant is a free-to-play tactical first-person shooter game developed and published by Riot Games.
+It was released for Microsoft Windows in June 2020. The game features two teams of five players each,
+with each player assuming the role of an agent with unique abilities and playstyles.\n
+Please choose what you want to see. \n`))
+
+console.log(chalk.green(`1. Characters`));
+console.log(chalk.green(`2. Skins`));
+console.log(chalk.green(`3. Maps`));
+console.log(chalk.green(`4. Chromas`));
+console.log(chalk.green(`5. Skin Levels`));
+console.log(chalk.green(`6. Equips`));
+console.log(chalk.green(`7. Game Modes`));
+console.log(chalk.green(`8. Sprays`));
+console.log(chalk.green(`9. Spray Levels`));
+console.log(chalk.green(`10.Player Cards`));
+console.log(chalk.green(`11.Player Titles`));
+console.log(chalk.green(`12.Ceremonies `));
+console.log(chalk.yellow(`exit: Exit the program\n`))
+console.groupEnd();
